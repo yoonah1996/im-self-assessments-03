6fa37eea7ec9c14f8e8873b22e0f486da8f7143f
yoonah1996 <yoonah1996@gmail.com>
2020-09-28 15:08:46 +0900

    complete

--- src/FishTable.js
+++ src/FishTable.js
@@ -3,4 +3,28 @@ import FishTableRow from "./FishTableRow";
 
 //TODO: FishTable will have many FishTableRows
 
+class FishTable extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            fishes : this.props.fishes
+        };
+    }
+    
+
+    render(){
+        return(
+            <div>
+                {this.state.fishes.map(el => (
+                    <FishTableRow 
+                    fish = {el} 
+                    setState = {this.fishTableRow}
+                    />
+
+                ))}
+            </div>
+        )
+    }
+}
+
 export default FishTable;
--- src/FishTableRow.js
+++ src/FishTableRow.js
@@ -2,4 +2,30 @@ import React, { Component } from "react";
 
 //TODO: FishTable has many FishTableRows
 
+class FishTableRow extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            fish : this.props.fish
+        };
+    }
+    
+
+    render(){
+        return(
+            <div>
+        <div>{this.state.fish.name}</div>
+        <div>
+            <img
+				src={this.state.fish.image}>
+            </img>
+        </div>
+        <div>
+            {this.state.fish.description}
+        </div>
+    </div>
+        )
+    }
+}
+    
 export default FishTableRow;
